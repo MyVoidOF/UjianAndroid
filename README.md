85cdba8f2c245921c43d5e8af8a72d2efe9af1d2
Zalfa Destian Ramadhani <voidoofrzzz@gmail.com>
2023-05-17 19:56:26 +0700

    Zalfa Destian Ramadhani G.211.20.0076

---
+++ README.md
@@ -0,0 +1,7 @@
+# APLIKASI ANDROID
+###### Aplikasi android sederhana untuk menampilkan inputan data kedalam listview
+
+### Dibuat Oleh :
+##### Zalfa Destian Ramadhani
+### NIM
+##### G.111.20.0076
---
+++ app/src/main/java/com/example/ujianandroid/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.ujianandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText edNamaDepan = (EditText) findViewById(R.id.edNamaDepan);
+        EditText edNamaBelakang = (EditText) findViewById(R.id.edNamaBelakang);
+        Button btnSimpan = (Button) findViewById(R.id.btnSimpan);
+
+        ArrayList<String> daftar_nama = new ArrayList<>();
+
+        Intent intent_list = new Intent(MainActivity.this, ListActivity.class);
+
+        btnSimpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String isian_nama_depan = edNamaDepan.getText().toString();
+                String isian_nama_belakang = edNamaBelakang.getText().toString();
+
+                if(isian_nama_depan.isEmpty()){
+                    Toast.makeText(getApplicationContext(), "Isian masih kosong", Toast.LENGTH_SHORT).show();
+                }else{
+                    String nama_lengkap = isian_nama_depan.concat(" ").concat(isian_nama_belakang);
+                    daftar_nama.clear();
+                    daftar_nama.add(nama_lengkap);
+                    edNamaDepan.setText("");
+                    edNamaBelakang.setText("");
+                    intent_list.putStringArrayListExtra("daftar_nama", daftar_nama);
+                    startActivity(intent_list);
+                }
+            }
+        });
+    }
+}
